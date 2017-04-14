0010a8392bebf281af5b1a5bec3225b7b8fc3cd4
吳豪杰 <kent19871014@gmail.com>
2017-04-14 15:27:19 +0800

    11 練習 Angular 元件通訊：透過 @Input() 宣告傳入屬性

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  @Input()
   title = "The Will Will Web"; 
   is_h3_highlight = false;
   counter = 0;
+  
   constructor() { }
 
   ngOnInit() {
